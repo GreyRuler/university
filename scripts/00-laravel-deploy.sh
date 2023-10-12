586ad9705c5fdbf3080f91425fa93b38debe44e2
GreyRuler <daniil_vasilenko757@mail.ru>
2023-10-12 23:37:40 +0300

    change script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+echo "Install nodejs"
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
 
+echo "Running composer"
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
-ls -l
+echo "Running install requirements for backend"
+npm install
+npm run build
+
+echo "Running install requirements for frontend"
+cd ./resources/js
+npm install
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Key generate..."
+php artisan key:generate --show
