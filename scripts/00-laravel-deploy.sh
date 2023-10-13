c868988a2cb1f6ffc6c4b159852d9582c808e4da
GreyRuler <daniil_vasilenko757@mail.ru>
2023-10-13 11:19:42 +0300

    change script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-ls -la
+#echo "Running composer"
+#composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
-echo "Running composer"
-composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+composer -v
+php -v
+npm -v
 
-ls -la
-
-echo "Running install requirements for backend"
-npm install
-npm run build
-
-echo "Running install requirements for frontend"
-cd ./resources/js
-npm install
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Key generate..."
-php artisan key:generate --show
+#echo "Running install requirements for backend"
+#npm install
+#npm run build
+#
+#echo "Running install requirements for frontend"
+#cd ./resources/js
+#npm install
+#
+#echo "Caching config..."
+#php artisan config:cache
+#
+#echo "Caching routes..."
+#php artisan route:cache
+#
+#echo "Key generate..."
+#php artisan key:generate --show
